2662c56c8c53ea96ce5bc57dc66a36bf0a301141
arth0056 <arth0056@stud.kea.dk>
2020-01-07 15:42:05 +0100

    fucking_done mangler evt finpudsning

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-window.addEventListener("load", sidenVises);
-
-function sidenVises() {
-    console.log("siden vises");
-    document.querySelector("#burgerknap").addEventListener("click", clickMenu);
-    document.querySelector("#forside_bg").addEventListener("click", Grow);
-}
-
-
-function clickMenu() {
-    console.log("clickMenu");
-    document.querySelector("#burgermenu").classList.toggle("hidden");
-    document.querySelector("#burgermenu").classList.toggle("fade_away");
-
-    if (document.querySelector("#burgerknap").classList.contains("open")) {
-        document.querySelector("#burgerknap").classList = "closed";
-    } else {
-        document.querySelector("#burgerknap").classList = "open";
-    }
-}
-
-
-function Grow() {
-    this.classList.add("grow");
-}
+//window.addEventListener("load", sidenVises);
+//
+//function sidenVises() {
+//    console.log("siden vises");
+//    document.querySelector("#burgerknap").addEventListener("click", clickMenu);
+//    document.querySelector("#forside_bg").addEventListener("click", Grow);
+//}
+//
+//
+//function clickMenu() {
+//    console.log("clickMenu");
+//    document.querySelector("#burgermenu").classList.toggle("hidden");
+//    document.querySelector("#burgermenu").classList.toggle("fade_away");
+//
+//    if (document.querySelector("#burgerknap").classList.contains("open")) {
+//        document.querySelector("#burgerknap").classList = "closed";
+//    } else {
+//        document.querySelector("#burgerknap").classList = "open";
+//    }
+//}
+//
+//
+//function Grow() {
+//    this.classList.add("grow");
+//}
